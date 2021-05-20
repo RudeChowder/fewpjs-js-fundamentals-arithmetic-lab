19ef007118ef47744941d79ea2eda208e2922f25
Stephen Crane <scrane88@gmail.com>
2021-05-20 19:03:00 -0400

    I did the math

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 =  31,
+      num2 = 2,
+      multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 14,
+      num4 = 5,
+      mod = num3 % num4;
+
+const max = Math.max(1,2,3,13,7,14,3,20)
